6bdeb6453500dd82406597e7f531dff28ab7d838
xiaozheng82828-lang <xiaozheng82828@gmail.com>
2025-12-30 20:53:00 +0530

    Create authMiddleware.js

---
+++ models/routes/middleware/authMiddleware.js
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+
+export default function(req,res,next){
+  const token = req.headers.authorization?.split(" ")[1];
+  if(!token) return res.status(401).json({error:"No token"});
+
+  try{
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.id;
+    next();
+  }catch(err){
+    res.status(401).json({error:"Invalid token"});
+  }
+}
